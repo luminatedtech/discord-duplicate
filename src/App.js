4398db2d5d9082d158967b1f1d5e3f7a73f5d904
Allen Kim <avalmercafe02@gmail.com>
2023-08-10 22:31:33 -0400

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-
+import SideBar from './SideBar';
 function App() {
   return (
     <div className="flex">
-        Hello
+        <SideBar/>
     </div>
   );
 }
---
+++ src/SideBar.js
@@ -0,0 +1,19 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs'
+import {FaFire, FaPoo} from 'react-icons/fa'
+function SideBar () {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-sm">
+            <SideBarIcon icon={<FaFire size="28"/>}/>
+            <SideBarIcon icon={<FaPoo size="28"/>}/>
+            <SideBarIcon icon={<BsPlus size="28"/>}/>
+            <SideBarIcon icon={<BsGearFill size="28"/>}/>
+        </div>
+    )
+
+}
+const SideBarIcon = ({ icon })=> (
+    <div className='relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto bg-gray-50 text-green-300 hover:bg-green-600'>
+        {icon}
+    </div>
+)
+export default SideBar
