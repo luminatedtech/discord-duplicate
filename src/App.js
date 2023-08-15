cee753ece2ef959d0350f0300fb26482ab99c131
Allen Kim <avalmercafe02@gmail.com>
2023-08-15 09:59:39 -0400

    channelbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import SideBar from './SideBar';
+import ChannelBar from './ChannelBar';
+import ContentContainer from './ContentContainer';
 function App() {
   return (
     <div className="flex">
         <SideBar/>
+        <ChannelBar/>
+        <ContentContainer/>
     </div>
   );
 }
